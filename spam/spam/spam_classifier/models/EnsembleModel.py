8ae09a436d3b314824129c5707c3ad6991d341c2
nill1024 <nill2114@gmail.com>
2020-07-31 17:02:26 +0900

    final

--- spam/spam/spam_classifier/models/EnsembleModel.py
+++ spam/spam/spam_classifier/models/EnsembleModel.py
@@ -33,52 +33,52 @@ class EnsembleModel:
 
     def fit(self, epochs_finetune, epochs_full, batch_size, debug=False):
         self.debug = debug
-        self.data.prepare() # rearrange와 디렉토리 초기화
+        # self.data.prepare() # rearrange와 디렉토리 초기화
 
-        # 이 과정까지 진행했을 때 디렉토리가 정렬이 되기 때문에 이시점에서 언더샘플링 하던지
+        # # 이 과정까지 진행했을 때 디렉토리가 정렬이 되기 때문에 이시점에서 언더샘플링 하던지
 
-        self.net1.compile(
-            loss=self.loss(),
-            optimizer=self.optimizer('finetune'),
-            metrics=self.fit_metrics()
-        )
+        # # self.net1.compile(
+        # #     loss=self.loss(),
+        # #     optimizer=self.optimizer('finetune'),
+        # #     metrics=self.fit_metrics()
+        # # )
 
-        steps_per_epoch_train = int(self.data.len('train') / batch_size) if not self.debug else 2
+        # steps_per_epoch_train = int(self.data.len('train') / batch_size) if not self.debug else 2
         
-        train_gen, val_gen, unl_gen, unl_files = self.data.train_val_gen(batch_size) # 이 부분이 train data가 들어오는 부분임
+        # train_gen, val_gen, unl_gen, unl_files = self.data.train_val_gen(batch_size) # 이 부분이 train data가 들어오는 부분임
         
         #nsml.save(checkpoint='pretuned')# 근데 이게 왜 두개 있는지..?
 
         # 이 부분에서 데이터 수정이 들어가야 할 것 같다 아마도
         # fit generator 그냥 학습 시키는 거다. 그니까 여기가 사실상 fit 부분임
 
-        model_path_finetune = 'model_finetuned.h5'
-        self.net1.fit_generator(generator=train_gen, #참고: 이미 network가 resnet 객체인고로 필요없당.
-                                   steps_per_epoch=steps_per_epoch_train,
-                                   epochs=1,
-                                   callbacks=self.callbacks_ft(
-                                       model_path=model_path_finetune,
-                                       model_prefix='last_layer_tuning',
-                                       patience=5,
-                                       val_gen=val_gen,
-                                       classes=self.data.classes),
-                                   validation_data=val_gen,
-                                   use_multiprocessing=True,
-                                   workers=20)  # TODO change to be dependent on n_cpus
-        self.net1.load_weights(model_path_finetune)
+        # model_path_finetune = 'model_finetuned.h5'
+        # self.net1.fit_generator(generator=train_gen, #참고: 이미 network가 resnet 객체인고로 필요없당.
+        #                            steps_per_epoch=steps_per_epoch_train,
+        #                            epochs=1,
+        #                            callbacks=self.callbacks_ft(
+        #                                model_path=model_path_finetune,
+        #                                model_prefix='last_layer_tuning',
+        #                                patience=5,
+        #                                val_gen=val_gen,
+        #                                classes=self.data.classes),
+        #                            validation_data=val_gen,
+        #                            use_multiprocessing=True,
+        #                            workers=20)  # TODO change to be dependent on n_cpus
+        # self.net1.load_weights(model_path_finetune)
         
-        # finetuning 먼저 하는 듯
-        # finetuning은 초기값을 주기 위해서 하는 과정이라고 보면 된다.
-        # 합성곱 신경망의 미세조정(finetuning): 무작위 초기화 대신, 신경망을 ImageNet 1000 데이터셋 등으로 미리 학습한 신경망으로 초기화합니다. 
-        # 학습의 나머지 과정들은 평상시와 같습니다.
+        # # finetuning 먼저 하는 듯
+        # # finetuning은 초기값을 주기 위해서 하는 과정이라고 보면 된다.
+        # # 합성곱 신경망의 미세조정(finetuning): 무작위 초기화 대신, 신경망을 ImageNet 1000 데이터셋 등으로 미리 학습한 신경망으로 초기화합니다. 
+        # # 학습의 나머지 과정들은 평상시와 같습니다.
 
-        self.unfreeze()
+        # self.unfreeze()
 
-        self.net1.compile(
-            loss=self.loss(),
-            optimizer=self.optimizer('full'),
-            metrics=self.fit_metrics()
-        )
+        # self.net1.compile(
+        #     loss=self.loss(),
+        #     optimizer=self.optimizer('full'),
+        #     metrics=self.fit_metrics()
+        # )
 
         # model_path_full = 'model_full.h5'
         # self.net1.fit_generator(generator=train_gen,
@@ -101,45 +101,46 @@ class EnsembleModel:
         nsml.load(checkpoint='full',session='nill1024/spam-3/35')
         nsml.load(checkpoint='full',session='nill1024/spam-1/53')
         nsml.load(checkpoint='best',session='nill1024/spam-1/10')
+        nsml.load(checkpoint='full',session='nill1024/spam-3/66')
         
-        unl_pred1 = self.net2.predict_generator(unl_gen)
-        unl_pred2 = self.net3.predict_generator(unl_gen)
-        unl_pred3 = self.net1.predict_generator(unl_gen)
+        # unl_pred1 = self.net2.predict_generator(unl_gen)
+        # unl_pred2 = self.net3.predict_generator(unl_gen)
+        # unl_pred3 = self.net1.predict_generator(unl_gen)
 
-        unl_pred = (unl_pred1+unl_pred2+unl_pred3)/3
-        arg_unl = np.argmax(unl_pred,axis=1)
+        # unl_pred = (unl_pred1+unl_pred2+unl_pred3)/3
+        # arg_unl = np.argmax(unl_pred,axis=1)
 
-        print("prediction completed")
-        for i in range(len(arg_unl)):
-            if unl_pred[i][arg_unl[i]] < 0.95:
-                arg_unl[i] = -1
+        # print("prediction completed")
+        # for i in range(len(arg_unl)):
+        #     if unl_pred[i][arg_unl[i]] < 0.95:
+        #         arg_unl[i] = -1
 
-        df_u = pd.DataFrame({'filename': unl_files, 'unl_pred': arg_unl})
+        # df_u = pd.DataFrame({'filename': unl_files, 'unl_pred': arg_unl})
         
-        print(df_u.head())
+        # print(df_u.head())
 
-        unl_train, unl_val = self.data.labeld_unl(df_u,batch_size)
+        # unl_train, unl_val = self.data.labeld_unl(df_u,batch_size)
 
-        model_path_full = 'model_full.h5'
-        self.net1.fit_generator(generator=unl_train,
-                            steps_per_epoch=steps_per_epoch_train,
-                            epochs=epochs_full,
-                            callbacks=self.callbacks(
-                                model_path=model_path_full,
-                                model_prefix='full_tuning',
-                                val_gen=val_gen, # 이거 일부러 unl_val은 안쓴다. 참고
-                                patience=10,
-                                classes=self.data.classes),
-                            validation_data=val_gen,
-                            use_multiprocessing=True,
-                            workers=20)
-        self.net1.load_weights(model_path_full)
+        # model_path_full = 'model_full.h5'
+        # self.net1.fit_generator(generator=unl_train,
+        #                     steps_per_epoch=steps_per_epoch_train,
+        #                     epochs=epochs_full,
+        #                     callbacks=self.callbacks(
+        #                         model_path=model_path_full,
+        #                         model_prefix='full_tuning',
+        #                         val_gen=val_gen, # 이거 일부러 unl_val은 안쓴다. 참고
+        #                         patience=10,
+        #                         classes=self.data.classes),
+        #                     validation_data=val_gen,
+        #                     use_multiprocessing=True,
+        #                     workers=20)
+        # self.net1.load_weights(model_path_full)
 
         nsml.save(checkpoint='full') #이거 부를 때마다 모델 체크포인트를 남길 수 있는데 나중에 가면 많이 써야할 것 같음.
         #아마 콜백이 있어서 기존 체크포인트가 best였던 모양인데 원래 콜백은 그냥 기본이라고 볼 수 있으므로 full
 
         print('Done')
-        self.metrics(gen=val_gen)
+        # self.metrics(gen=val_gen)
 
     def unfreeze(self) -> None:
         for layer in self.net1.layers:
@@ -202,7 +203,7 @@ class EnsembleModel:
         ]
         return callbacks
 
-    def evaluate(self, test_dir: str, vote=False) -> pd.DataFrame:
+    def evaluate(self, test_dir: str, vote=True) -> pd.DataFrame:
         """
 
         이거 오로지 테스트에 쓰이는 함수라는거
@@ -220,14 +221,14 @@ class EnsembleModel:
         y_pred1 = self.net1.predict_generator(gen) # 아무래도 fit된 모델 기반으로 예측값을 만드는 것 같음. 이걸 기준으로 다시 모델에 피드백시킬 수 있을 것 같다.(특히 unlabeled data)
         y_pred2 = self.net2.predict_generator(gen)
         y_pred3 = self.net3.predict_generator(gen)
-        # y_pred4 = self.net4.predict_generator(gen)
+        y_pred4 = self.net4.predict_generator(gen)
 
         y_average = (y_pred1+y_pred2+y_pred3) #모델 하나 더 추가할것. 우선은 voting만 만듬
 
         p1 = np.argmax(y_pred1,axis=1)
         p2 = np.argmax(y_pred2,axis=1)
         p3 = np.argmax(y_pred3,axis=1)
-        # p4 = np.argmax(y_pred4,axis=1)
+        p4 = np.argmax(y_pred4,axis=1)
 
         y_pred = np.argmax(y_average, axis=1)
 
@@ -240,7 +241,7 @@ class EnsembleModel:
             ma[p1[i]] += 1
             ma[p2[i]] += 1
             ma[p3[i]] += 1
-            # ma[p4[i]] += 1 #efn3 에 1표를 다 주기 때문에 조금 안좋아 지는듯 함. 아무래도 단일 모델 성능이 안좋은 경우면 앙상블에서 voting방식의 성능에 영향을 줄 수 있음
+            ma[p4[i]] += 1 #efn3 에 1표를 다 주기 때문에 조금 안좋아 지는듯 함. 아무래도 단일 모델 성능이 안좋은 경우면 앙상블에서 voting방식의 성능에 영향을 줄 수 있음
                             #이 점 때문에 voting에서 한표를 다 안주는 방식으로 알고리즘을 짜게 되면, 기본적으로 average방식과 그 알고리즘이 유사해짐. average도 가중치를 주고 할 수 있는 거라
                             #물론 얘를 가중치 주는거랑 저 위에걸 가중치 주는거랑은 살짝 다르긴 함. cross_entropy 때문에
             mx = 0
@@ -300,18 +301,17 @@ def bind_model(model: EnsembleModel):
             elif str(Path(f'{dirname}/model').stat().st_size)[0] == '1':
                 model.net3.load_weights(f'{dirname}/model')
                 print("net 3 loaded")
-            # elif str(Path(f'{dirname}/model').stat().st_size)[0] == '9':
-            #     model.net1.load_weights(f'{dirname}/model')
-            #     print("net 1 loaded")
-            elif str(Path(f'{dirname}/model').stat().st_size)[0] == '9': #현재 resnet이 net4로 로드됨
-                #model.net4.load_weights(f'{dirname}/model')
-                model.net1.load_weights(f'{dirname}/model')
+            elif str(Path(f'{dirname}/model').stat().st_size)[0] == '9':
+                model.net4.load_weights(f'{dirname}/model')
+                print("net 4 loaded")
+            elif str(Path(f'{dirname}/model').stat().st_size)[0] == '4': #현재 resnet이 net4로 로드됨
+                model.net1.load_weights(f'{dirname}/model1')
                 print("net 1 loaded")
         except:
             model.net1.load_weights(f'{dirname}/model1')
             model.net2.load_weights(f'{dirname}/model2')
             model.net3.load_weights(f'{dirname}/model3')
-            #model.net4.load_weights(f'{dirname}/model4')
+            model.net4.load_weights(f'{dirname}/model4')
 
 
     def save(dirname, **kwargs):
@@ -322,8 +322,8 @@ def bind_model(model: EnsembleModel):
         model.net2.save_weights(filename+'2')
         print(f'Trying to save to {filename}'+'3')
         model.net3.save_weights(filename+'3')
-        # print(f'Trying to save to {filename}'+'4')
-        # model.net4.save_weights(filename+'4')
+        print(f'Trying to save to {filename}'+'4')
+        model.net4.save_weights(filename+'4')
 
     def infer(test_dir, **kwargs):
         return model.evaluate(test_dir)
